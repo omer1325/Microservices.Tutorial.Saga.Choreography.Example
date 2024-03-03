17db4798d8765d8d21cfa55557096458f6207096
omer1325 <omer.ingec24@gmail.com>
2024-03-03 23:28:39 +0300

    Add project files.

---
+++ Order.API/Models/Contexts/OrderAPIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Order.API.Models.Contexts
+{
+    public class OrderAPIDbContext : DbContext
+    {
+        public OrderAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
